129704f930f0b79d9703aeee757b50088e119464
ydnactnim <ydnactnim@gmail.com>
2024-09-27 14:49:43 +0900

    complete iceCreamStrore.js
    complete iceCreamStrore.js

--- iceCreamStore.js
+++ iceCreamStore.js
@@ -4,7 +4,7 @@ let iceCreamFlavors = [
   { name: "Vanilla", type: "Vanilla", price: 2 },
   { name: "Pistachio", type: "Nuts", price: 1.5 },
   { name: "Neapolitan", type: "Chocolate", price: 2 },
-  { name: "Mint Chip", type: "Chocolate", price: 1.5 },
+  { name: "MintChip", type: "Chocolate", price: 1.5 },
   { name: "Raspberry", type: "Fruit", price: 1 },
 ];
 
@@ -13,7 +13,7 @@ let transactions = [];
 
 // { scoops: [], total: }
 transactions.push({
-  scoops: ["Chocolate", "Vanilla", "Mint Chip"],
+  scoops: ["Chocolate", "Vanilla", "MintChip"],
   total: 5.5,
 });
 transactions.push({ scoops: ["Raspberry", "StrawBerry"], total: 2 });
@@ -21,16 +21,26 @@ transactions.push({ scoops: ["Vanilla", "Vanilla"], total: 4 });
 
 // 수익 계산
 const total = transactions.reduce((acc, curr) => acc + curr.total, 0);
-console.log(`You've made ${total} $ today`); // You've made 11.5 $ toda
+console.log(`You've made ${total} $ today`); // You've made 11.5 $ today
 
 // 각 맛의 판매량
+let max = 0;
+let max_Stuff = "Nothing";
+
 let flavorDistribution = transactions.reduce((acc, curr) => {
   curr.scoops.forEach((scoop) => {
     if (!acc[scoop]) {
       acc[scoop] = 0;
     }
     acc[scoop]++;
+
+    if (max < acc[scoop]) {
+      max = acc[scoop];
+      max_Stuff = scoop;
+    }
   });
   return acc;
-}, {}); // { Chocolate: 1, Vanilla: 3, Mint Chip: 1, Raspberry: 1, StrawBerry: 1 }
+}, {});
+
 console.log(flavorDistribution);
+console.log(`Best : ${max_Stuff}, Score : ${max}`);
